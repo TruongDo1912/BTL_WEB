0129a428655fe2122213346bc1f84a8a4665fadd
doxua <doxua@DESKTOP-SKDVC3M>
2024-01-16 10:00:51 +0700

    Add project files.

---
+++ BTL_WEB/Controllers/DetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_WEB.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
