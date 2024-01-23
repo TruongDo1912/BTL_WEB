d4dbd33114d3eb8b8700c70a08ba6f417dc4965e
doxua <doxua@DESKTOP-SKDVC3M>
2024-01-23 17:33:58 +0700

    Update

--- BTL_WEB/Controllers/ShopController.cs
+++ BTL_WEB/Controllers/ShopController.cs
@@ -13,7 +13,7 @@ namespace BTL_WEB.Controllers
         private BANDONGHOEntities db = new BANDONGHOEntities();
 
         // GET: Shop
-        public ActionResult Index(int? page, string keyword)
+        public ActionResult Index(int? page, string keyword,string loai)
         {
             int pageSize = 6; // Số sản phẩm hiển thị trên mỗi trang
             int pageNumber = (page ?? 1);
@@ -27,6 +27,12 @@ namespace BTL_WEB.Controllers
                 //StringComparison.OrdinalIgnoreCase : hàm so sánh chữ hoa với chữ thường
                 .Where(s => s.TENSP.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0 || s.TENSP.Any(c => c.ToString().IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0))
                 .ToList();
+            }
+            if (!string.IsNullOrEmpty(loai))
+            {
+                danhSachSanPham = danhSachSanPham
+                    .Where(s => s.LOAISANPHAM.TENLOAISP == loai)
+                    .ToList();
             }
             // Phân Trang
             IPagedList<SANPHAM> pagedList = danhSachSanPham.ToPagedList(pageNumber, pageSize);
--- BTL_WEB/Controllers/UserController.cs
+++ BTL_WEB/Controllers/UserController.cs
@@ -11,9 +11,15 @@ namespace BTL_WEB.Controllers
     {
         private BANDONGHOEntities db = new BANDONGHOEntities();
         // GET: User
-        public ActionResult Index()
+        public ActionResult Index(string loai)
         {
             List<SANPHAM> danhSachSanPham = db.SANPHAMs.Take(8).ToList();
+            if (!string.IsNullOrEmpty(loai))
+            {
+                danhSachSanPham = danhSachSanPham
+                    .Where(s => s.LOAISANPHAM.TENLOAISP == loai)
+                    .ToList();
+            }
             return View(danhSachSanPham);
         }
        
